72c1651c28536965d7318a8aacb685f505ef9d7e
Matias Da Silva <joel.dasilva2015@outlook.com>
2024-08-09 19:20:45 -0300

    Make search functional

--- projects/third-class_project/src/Api.jsx
+++ projects/third-class_project/src/Api.jsx
@@ -21,22 +21,19 @@ const listApi = [
 ]
 
 export function SearchApi(){
-    const { dirApi } = useParams()
-    console.log("esto es dirApi: ", dirApi);
-    const listadoApi = listApi.filter(elemento => elemento.name == dirApi)
+    let { dirApi } = useParams()
+    const listadoApi = listApi.filter(elemento => elemento.name == dirApi) // Nueva lista con api coincidentes con la búsqueda.
 
     function checkMatch(){
         if (!listadoApi.length) {
             return (
-                <div className="api-container-list-element prueba">
-                    <p>{"Sin resultados"}</p>
-                </div>
+                <p>{"Sin resultados"}</p>
                 )
         } else {
             return (listadoApi.map((value, index) => {
                 return (
-                    <div className="api-container-list-element prueba">
-                        <Link key={dirApi} to={value.link}>{value.name}</Link>
+                    <div key={dirApi} className="api-container-list-element prueba">
+                        <Link to={value.link}>{value.name}</Link>
                     </div>
                     )
             }))
@@ -51,12 +48,25 @@ export function SearchApi(){
 
 export function Api(){
     const [ url, setUrl ] = useState("");
+    const [ titleName, setTitleName ] = useState("Listado de Apis:");
+    let { dirApi } = useParams()
+    const [ listApiVisibility, setListApiVisibility ] = useState("");
+
+    useEffect(()=>{
+        if (dirApi){
+            setTitleName("Resultado de busqueda:");
+            setListApiVisibility(" hidden");
+        }else {
+            setTitleName("Listado de Apis:");
+            setListApiVisibility("");
+        }
+    },[dirApi])
 
     return (
         <main className="main">
             <section className="api-container">
                 <div className="api-container-header">
-                    <h2 className="api-container-header-title">Listado de Apis:</h2>
+                    <h2 className="api-container-header-title">{titleName}</h2>
                     <div className="api-container-header-search">
                         <input className="api-container-header-search-box" 
                                 type="text" 
@@ -64,13 +74,14 @@ export function Api(){
                                 id="searchApi" 
                                 placeholder="Buscar API" 
                                 onInput={(e)=>{setUrl(e.target.value)}}></input>
-                        <Link className="api-container-header-search-btn" to={`/api/${url}`}>Search</Link>
+                        <Link key={url+toString(Math.floor(Math.random()*100))} className="api-container-header-search-btn" to={`/api/${url}`}>Search</Link>
                     </div>
                 </div>
-                <Outlet/>
+                <div className="api-container-list search">
+                    <Outlet/>
+                </div>
 
-
-                <div className="api-container-list">
+                <div className={"api-container-list search"+listApiVisibility}>
                     {listApi.map((value, index) => {
                         return(
                             <div className="api-container-list-element" key={value.id}>
